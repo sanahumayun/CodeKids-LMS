5e1617a2546722c150dae4e64af2b4a613c0d7d1
sanahumayun <138251587+sanahumayun@users.noreply.github.com>
2026-01-09 10:43:07 +0500

    Update index.js

--- Project/server/index.js
+++ Project/server/index.js
@@ -29,9 +29,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// IMPORTANT: explicitly handle preflight
-app.options('*', cors());
-
+app.options('/*', cors());
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
